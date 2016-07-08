e0fa2f78a69616c282b8e26d769265c10b398a02
darui00kara <Takeshi00Miyamoto@gmail.com>
2016-07-08 16:16:33 +0900

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule UnitTest.Mixfile do
+defmodule Kinotake.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :unit_test,
+    [app: :kinotake,
      version: "0.0.1",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
